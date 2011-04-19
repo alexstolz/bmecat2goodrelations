21923f66b122f75f2403e9fff4c75fcaf3e35b35
Alex Stolz <alex.stolz@ebusiness-unibw.org>
2011-04-19 21:48:31 +0200

    buggy html out

--- specgen.py
+++ specgen.py
@@ -17,15 +17,29 @@ import os
 
 FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 
-def get_subElements(g, e, t):
-    """Get a list of all subelements that belong to the given element"""
-    subElements = []
-    for se in g.subjects(t, e): # has subElements
-        if type(se) is URIRef:
-            se_uri = str(se)
-            subElements.append(se_uri)
-    subElements.sort(lambda a, b: cmp(a.lower(), b.lower()))
-    return subElements
+def get_isSubElementOf(g, e, t):
+    """Get a list of all superelement URIs of a given element"""
+    e = URIRef(e)
+    se = g.value(e, t)
+    se_list = []
+    
+    if type(se) is not URIRef: # subElementof is a collection
+        for se_outer_element in g.objects(e, t): # get OWL:Class
+            if type(se_outer_element) is BNode:
+                for se_collection in g.objects(se_outer_element, OWL.unionOf):
+                    superElements = list(g.items(se_collection))
+                    for s in superElements:
+                        s_uri = str(s)
+                        se_list.append(s_uri)
+        se = se_list
+    else: # subelementof is no collection, but a sequence of "rdfs:subClassOf rdf:resource=..."
+        for i in g.objects(e, t): # get all superElements of e
+            if type(i) is URIRef:
+                i_uri = str(i)
+                se_list.append(i_uri)
+        se = se_list
+    
+    return se
 
 def pretty_list(l):
     """Bring the passed list to a suitable format and return the formatted string with anchor links"""
@@ -34,7 +48,9 @@ def pretty_list(l):
         l.sort() # sort the list alphabetically
         for item in l:
             # navigation in html document, if VOCAB ontology base uri is found
-            item_idf = item[item.find("#"):]
+            item_idf = item
+            if not "http://purl.org/goodrelations/v1#" in item:
+                item_idf = item[item.find("#"):]
             pretty_format += " <a href=\"%s\">%s</a>" % (item_idf, item_idf)
     return pretty_format
 
@@ -61,13 +77,15 @@ def create_html(output_dir):
             uri = str(c)
             idf = uri[uri.find("#")+1:]
             label = g.value(c, RDFS.label)
+            title = str(label)
+            title = title[:title.find("(")].strip()
             comment = g.value(c, RDFS.comment)
-            subclassof = get_subElements(g, c, RDFS.subClassOf)
+            subclassof = get_isSubElementOf(g, c, RDFS.subClassOf)
             depiction = g.value(c, FOAF.depiction)
             if depiction:
                 depiction = """<img src="%(depiction)s" alt="" />""" % ({"depiction":depiction, "label":label})
-            classes.append({"idf":idf, "label":label, "type_url":OWL.Class, "type":"owl:Class", "uri":uri, "depiction":depiction,
-              "rdfs_subclassof":RDFS.subClassOf, "rdfs_comment":RDFS.comment, "comment":comment, "subclassof":pretty_list(subclassof)})
+            classes.append({"idf":idf, "title":title, "label":label, "type_url":OWL.Class, "type":"owl:Class", "uri":uri, "depiction":depiction,
+              "rdfs_subclassof":RDFS.subClassOf, "rdfs_comment":RDFS.comment, "rdfs_label":RDFS.label, "comment":comment, "subclassof":pretty_list(subclassof)})
                 
     html.write(template_classes.render({"classes":classes}).encode("utf-8"))
     
